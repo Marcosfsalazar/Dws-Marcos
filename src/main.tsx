8db4eb1bc7ef27b3a49c852ed0032d1898fab620
Marcos Salazar <marcos1010521@gmail.com>
2025-01-24 16:13:46 -0400

    feat: theme config

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './components/themes/default.ts';
 
 const queryClient = new QueryClient();
 
@@ -11,7 +13,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={defaultTheme}>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
